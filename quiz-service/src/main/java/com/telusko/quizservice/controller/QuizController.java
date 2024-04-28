a917d5742f65e68f921b331e6cfa197161523fd0
akshay <baviskarakshay0702@gmail.com>
2024-04-28 13:16:58 +0530

    added: comments

--- quiz-service/src/main/java/com/telusko/quizservice/controller/QuizController.java
+++ quiz-service/src/main/java/com/telusko/quizservice/controller/QuizController.java
@@ -1,7 +1,6 @@
 package com.telusko.quizservice.controller;
 
 
-import com.netflix.discovery.converters.Auto;
 import com.telusko.quizservice.dto.QuizDto;
 import com.telusko.quizservice.model.QuestionWrapper;
 import com.telusko.quizservice.model.Quiz;
@@ -23,33 +22,58 @@ public class QuizController {
     @Autowired
     private Environment environment;
 
+
+    /**
+     * This method creates a quiz with the given category, sets the number of questions and assigns title
+     * @param category The category of the quiz
+     * @param numQ The number of questions
+     * @param title The title of the quiz
+     * @return ResponseEntity with the success message and status code
+     */
     @PostMapping("create")
     public ResponseEntity<String> createQuiz(@RequestBody QuizDto quizDto){
         System.out.println(environment.getProperty("local.server.port"));
         return quizService.createQuiz(quizDto.getCategory(), quizDto.getNumQuestions(), quizDto.getTitle());
     }
 
-    // find quiz by id
+   /**
+    * This method returns quiz by its Id
+    * @param id The id of the quiz
+    * @return ResponseEntity with the quiz and status code'
+    */
     @GetMapping("get/{id}")
     public ResponseEntity<List<QuestionWrapper>> getQuizQuestion(@PathVariable Integer id){
         System.out.println(environment.getProperty("local.server.port"));
         return quizService.getQuizById(id);
     }
 
-    // submit the quiz
+    /**
+     * This method returns the score of the quiz
+     * @param id The id of the quiz
+     * @param responses The list of responses of the quiz
+     * @return ResponseEntity with the score and status code
+     */
     @PostMapping("submit/{id}")
     public ResponseEntity<Integer> submitQuiz(@PathVariable Integer id,
                                               @RequestBody List<Response> responses){
         return quizService.calculateResult(id, responses);
     }
 
-    //update quiz
+    /**
+     * This method updates the quiz or throws exception if quiz not found
+     * @param quiz The updated quiz
+     * @return ResponseEntity with the success message and status code
+     */
     @PutMapping("update")
     public ResponseEntity<String> updateQuiz(@RequestBody Quiz quiz){
         return quizService.updateQuiz(quiz);
     }
 
-    // delete quiz
+    /**
+     * This method deletes the quiz or throws exception if quiz not found
+     * @param id The id of the quiz
+     * @return ResponseEntity with the success message and status code
+     */
     @DeleteMapping("delete/{id}")
     public ResponseEntity<String> deleteQuiz(@PathVariable Integer id){
         return quizService.deleteQuiz(id);
