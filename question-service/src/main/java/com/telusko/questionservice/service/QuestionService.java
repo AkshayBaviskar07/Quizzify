e0fa7dea9016e3e96fbda3f35a8dbc66a799cac8
akshay <baviskarakshay0702@gmail.com>
2024-04-28 12:15:59 +0530

    added: comments

--- question-service/src/main/java/com/telusko/questionservice/service/QuestionService.java
+++ question-service/src/main/java/com/telusko/questionservice/service/QuestionService.java
@@ -21,11 +21,24 @@ public class QuestionService {
     private QuestionRepository repo;
 
 
+    /**
+     * Add a new question to database
+     * @param Question  a question object to be added
+     * @return ResponseEntity with a string value and status code
+     */
     public ResponseEntity<String> addQuestion(Question question){
+        // save question in database and return success message
         repo.save(question);
         return new ResponseEntity<String>("success", HttpStatus.CREATED);
     }
 
+
+    /**
+     * Get all questions from database
+     * @return ResponseEntity with a list of Question objects
+     * @return ResponseEntity with a list of Question objects and status code if questions are available
+     * @throws QuestionNotFoundException if questions are not present
+     */
     public ResponseEntity<List<Question>> getAllQuestion() {
         List<Question> questionList = repo.findAll();
 
@@ -38,6 +51,12 @@ public class QuestionService {
         }
     }
 
+    /**
+     * Get questions by category
+     * @param String category of the question
+     * @return ResponseEntity with a list of Question objects and status code if questions are available
+     * @throws QuestionNotFoundException if questions are not present
+     */
     public ResponseEntity<List<Question>> getQuestionsByCategory(String category) {
         List<Question> questions = repo.findByCategory(category);
 
@@ -49,6 +68,12 @@ public class QuestionService {
         }
     }
 
+    /**
+     * Update a question in database
+     * @param Question  a question object to be updated
+     * @return ResponseEntity with a string value and status code if question is updated
+     * @throws QuestionNotFoundException if questions is not present
+     */
     public ResponseEntity<String> updateQuestion(Question question) {
         Optional<Question> queOptional = repo.findById(question.getId());
 
@@ -61,6 +86,12 @@ public class QuestionService {
         }
     }
 
+    /**
+     * Delete a question from database
+     * @param Long id of the question to be deleted
+     * @return ResponseEntity with a string value and status code if question is deleted
+     * @throws QuestionNotFoundException if questions is not present
+     */
     public ResponseEntity<String> deleteQuestion(Long id) {
         Optional<Question> queOptional = repo.findById(id);
 
@@ -73,11 +104,23 @@ public class QuestionService {
         }
     }
 
+
+    /**
+     * Get a list of question IDs for a quiz
+     * @param categoryName The name of the category of questions to be included in the quiz
+     * @param numQuestions The number of questions to be included in the quiz
+     * @return A list of question IDs
+     */
     public List<Long> getQuestionsForQuiz(String categoryName, Long numQuestions) {
         List<Long> questionIds = repo.findRandomQuestionsByCategory(categoryName, numQuestions);
         return questionIds;
     }
 
+    /**
+     * Get a list of QuestionWrapper objects from a list of question IDs
+     * @param questionIds A list of question IDs
+     * @return A list of QuestionWrapper objects
+     */
     public List<QuestionWrapper> getQuestionsFromIds(List<Long> questionIds) {
         List<QuestionWrapper> wrappers = new ArrayList<>();
         List<Question> questions = new ArrayList<>();
@@ -100,6 +143,12 @@ public class QuestionService {
         return wrappers;
     }
 
+
+    /**
+     * Get the score of a list of responses
+     * @param responses A list of responses
+     * @return The score of the responses
+     */
     public Integer getScore(List<Response> responses) {
 
         int result = 0;
